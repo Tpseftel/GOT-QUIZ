4f2b640c5f94c460cbc953e29c32e6d39de34339
Theodoros Pseftelis <pseftelistheo@gmail.com>
2020-04-22 11:32:58 +0300

    update

--- main.js
+++ main.js
@@ -1,21 +1,78 @@
 // http://proto.io/en/jobs/candidate-questions/quiz.json
 // http://proto.io/en/jobs/candidate-questions/result.json
 // TODO: Use Local Storage for current question, score  and more
-let quiz ;
-let curQuest;
+
+const quizUrl = 'http://proto.io/en/jobs/candidate-questions/quiz.json' ;
+const resultsUrl = 'http://proto.io/en/jobs/candidate-questions/result.json' ;
+let quiz = {};
+let curQuest = {};
+let questions = [];
 
 
-async function getData() {
-    //FIXME: Get data with ajax call
-    try {
-        const quiz = await fetch("http://proto.io/en/jobs/candidate-questions/quiz.json");
-        return quiz;
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
+function getData() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response =  JSON.parse(this.responseText);
+            quiz = response;
+            questions = response.questions;
+            curQuest = questions[2];
+            renderMainUI(quiz,quiz.title, quiz.description);
+            renderQuestions(curQuest);
+        }
+    };
+    xhttp.open("GET", quizUrl, true);
+    xhttp.send();
+}
+getData();
+
+function renderMainUI(quiz, title, desc) {
+    document.getElementById("quiz").innerHTML = quiz;
+    document.getElementById("main-title").innerHTML = title;
+    document.getElementById("main-desc").innerHTML = desc;
+}
+
+function renderQuestions(question){
+    document.getElementById("qtitle").innerHTML = question.title;
+    document.getElementById("qimg").style.backgroundImage = question.img;
+    document.getElementById("qtype").innerHTML = question.question_type;
+    
+    // render answers
+    let div = document.getElementById("qanswers");
+    if (question.question_type == "mutiplechoice-single"){
+        let ol = document.createElement("OL");
+        let pAnswers = question.possible_answers;
+        pAnswers.forEach(answer => {
+            let li = document.createElement("LI");
+            let text = document.createTextNode(answer.caption);
+            li.appendChild(text);
+            ol.appendChild(li);
+            div.appendChild(ol);
+        });
+    } else if(question.question_type == "truefalse") {
+        let btn_true = document.createElement("Button");
+        btn_true.setAttribute("id", "1");
+        btn_true.innerHTML = "TRUE";
+        let btn_false = document.createElement("Button");
+        btn_false.setAttribute("id", "0");
+        btn_false.innerHTML = "FALSE" ;
+        div.appendChild([btn_true, btn_false]);
+    }else {
+        let ol = document.createElement("OL");
+        let pAnswers = question.possible_answers;
+        pAnswers.forEach(answer => {
+            let li = document.createElement("LI");
+            let text = document.createTextNode(answer.caption);
+            li.appendChild(text);
+            ol.appendChild(li);
+            div.appendChild(ol);
+        });
     }
 }
 
-function createUI(){
+
+
+function createUI(quiz, title, desc) {
     //TODO: For the current question must render the UI properly
     // Different Cases
     // mutiplechoice-single
@@ -25,11 +82,9 @@ function createUI(){
 
 function nextQuestion(){
     // TODO: Check if answer is valid
-   const validAnser =  validateAnswer(response);
-    if (answer  == true) {
-
-    }
-    
+//    const validAnser =  validateAnswer(response);
+//     if (answer  == true) {
+//     }
 }
 
 /**
@@ -43,15 +98,3 @@ function nextQuestion(){
 
     //  return true or false
  }
-
-
-// getData()
-//     .then(res => res.json())
-//     .then(data => {
-//         let main = document.getElementById("main");
-//         console.log(data);
-//         main.innerHTML = data.questions[0].possible_answers[0].caption;
-//     })
-//     .catch(error => {
-//         console.log(error.message);
-//     });
